7e354394379fb0aab332b84cb0db4c75987d0a8f
matlalipitso <matlalipitso6@gmail.com>
2025-10-25 04:19:55 +0200

    ..1818..

--- .history/backend/db_20251021141909.js
+++ .history/backend/db_20251021141909.js
@@ -4,12 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
-  user: process.env.DB_USER || '46uLvdkuDn4vp8d.root',
-  password: process.env.DB_PASSWORD || '1S6GYMwJsPF6nbT6',
-  database: process.env.DB_NAME || 'test',
-  port: process.env.DB_PORT || 4000,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'matlali@123',
+  database: process.env.DB_NAME || 'luct',
+  port: process.env.DB_PORT || 3306,
 });
+
 db.connect(err => {
   if (err) {
     console.error("Database connection failed:", err);
